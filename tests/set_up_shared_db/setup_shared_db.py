fbcc704c150814048715c44d79242dd74610c69d
ThuyHaLE <thuyha.le0590@gmail.com>
2025-06-20 00:06:54 +0700

    add__CI_workflow_4_OptiMoldIQ

--- tests/set_up_shared_db/setup_shared_db.py
+++ tests/set_up_shared_db/setup_shared_db.py
@@ -3,14 +3,14 @@ from agents.dataLoader.data_loader import DataLoaderAgent
 
 def test_data_collector_run():
     DataCollector(
-        source_dir="tests/mock_data/dynamicDatabase",
+        source_dir="tests/mock_database/dynamicDatabase",
         default_dir="tests/shared_db"
     )
     assert True
 
 def test_data_loader_run():
     DataLoaderAgent(
-        databaseSchemas_path="tests/mock_data/databaseSchemas.json",
+        databaseSchemas_path="tests/mock_database/databaseSchemas.json",
         annotation_path="tests/shared_db/DataLoaderAgent/newest/path_annotations.json",
         default_dir="tests/shared_db"
     )
