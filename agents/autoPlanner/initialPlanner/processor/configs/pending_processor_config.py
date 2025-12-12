6c5d70c0fbd6476b12b0246e8fb548aac2132872
ThuyHaLE <thuyha.le0590@gmail.com>
2025-12-12 23:51:58 +0700

    update_system_config_format

--- agents/autoPlanner/initialPlanner/processor/configs/pending_processor_config.py
+++ agents/autoPlanner/initialPlanner/processor/configs/pending_processor_config.py
@@ -40,16 +40,25 @@ class PendingProcessorConfig:
     log_progress_interval: Optional[int] = None
     use_sample_data: Optional[bool] = None
 
+    efficiency: Optional[float] = None
+    loss: Optional[float] = None
+
     # Default values
     MAX_LOAD_THRESHOLD = 30
     LOG_PROGRESS_INTERVAL = 5
     USE_SAMPLE_DATA = False
 
+    DEFAULT_EFFICIENCY = 0.85
+    DEFAULT_LOSS = 0.03
+
     def __post_init__(self):
         """Apply default values for None fields"""
         self.max_load_threshold = self._get_default(self.max_load_threshold, self.MAX_LOAD_THRESHOLD)
         self.log_progress_interval = self._get_default(self.log_progress_interval, self.LOG_PROGRESS_INTERVAL)
-        self.use_sample_data = self._get_default(self.use_sample_data, self.USE_SAMPLE_DATA)    
+        self.use_sample_data = self._get_default(self.use_sample_data, self.USE_SAMPLE_DATA)  
+
+        self.efficiency = self._get_default(self.efficiency, self.DEFAULT_EFFICIENCY)
+        self.loss = self._get_default(self.loss, self.DEFAULT_LOSS)  
 
     @staticmethod
     def _get_default(value, default):
