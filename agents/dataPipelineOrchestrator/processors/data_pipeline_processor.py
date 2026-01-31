3969feebc009e45868d7c5fb65c862b954bd8631
ThuyHaLE <thuyha.le0590@gmail.com>
2026-02-01 01:45:52 +0700

    update_agents__dataPipelineOrchestrator

--- agents/dataPipelineOrchestrator/processors/data_pipeline_processor.py
+++ agents/dataPipelineOrchestrator/processors/data_pipeline_processor.py
@@ -257,10 +257,7 @@ class DataPipelineProcessor(ConfigReportMixin):
             healing_success, recovery_result = self._process_healing_mechanism(
                 result_name=AgentType.SCHEMA_VALIDATOR, 
                 result_data=validation_result, 
-                notification_path=(
-                    f"{self.manual_review_notifications_dir}/{timestamp_file}_"
-                    f"{AgentType.SCHEMA_VALIDATOR.value}_manual_review_notification.txt"
-                ),
+                notification_path=f"{self.manual_review_notifications_dir}/{timestamp_file}_{AgentType.SCHEMA_VALIDATOR.value}_manual_review_notification.txt",
                 local_healer=SchemaErrorHealer
             )
             
@@ -334,10 +331,7 @@ class DataPipelineProcessor(ConfigReportMixin):
                 healing_success, recovery_result = self._process_healing_mechanism(
                     result_name=AgentType.DATA_COLLECTOR, 
                     result_data=collector_result, 
-                    notification_path=(
-                        f"{self.manual_review_notifications_dir}/{timestamp_file}_"
-                        f"{AgentType.DATA_COLLECTOR.value}_{db_type}_manual_review_notification.txt"
-                    ),
+                    notification_path=f"{self.manual_review_notifications_dir}/{timestamp_file}_{AgentType.DATA_COLLECTOR.value}_{db_type}_manual_review_notification.txt",
                     local_healer=DataCollectorHealer
                 )
 
